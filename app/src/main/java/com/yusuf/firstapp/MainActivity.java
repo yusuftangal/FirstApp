8defd4a535754464de07a21ad18900ad7478f6f8
Yusuf Tangaloğlu <yusuf.tangaloglu.5@gmail.com>
2022-11-05 20:41:10 +0300

    FirstApp
    İlk Java Resim Değiştirme App'm

---
+++ app/src/main/java/com/yusuf/firstapp/MainActivity.java
@@ -0,0 +1,27 @@
+package com.yusuf.firstapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+
+public void changeImage(View view) {
+    ImageView imageView = findViewById(R.id.imageView);
+    imageView.setImageResource(R.drawable.duman2);
+}
+
+
+
+}
